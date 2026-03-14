d847fe71a985fc88cd1c2591b26fecf153f92db7
Jasper <jmcclendon8383@gmail.com>
2026-03-14 10:58:27 -0500

    track layout update

--- ship.gd
+++ ship.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-const enginepower = 5
+const enginepower = 7
 const turnSpeed = 0.5
 var speed = 0
 var turnVel = 0
@@ -10,7 +10,13 @@ var enginerpm = 0
 func getInput(delta: float):
 	var moveInput = Input.get_axis("backward", "forward")
 	var turnInput = Input.get_axis("turnRight", "turnLeft")
-	enginerpm += moveInput/100
+	if is_on_wall():
+		drift += get_wall_normal()*speed*30
+		enginerpm *= 0.9
+		rotation.z = move_toward(rotation.z, get_wall_normal().x+get_wall_normal().z,delta)
+	if Input.is_action_just_pressed("forward"):
+		drift = Vector3.ZERO
+	enginerpm += moveInput/120
 	speed = enginerpm * enginepower
 	velocity = -transform.basis.z * speed
 	drift += velocity
@@ -18,15 +24,12 @@ func getInput(delta: float):
 	turnVel += turnSpeed * turnInput
 	rotate_y(turnVel * delta)
 	turnVel *= 0.8
-	drift *= 0.95
+	drift *= 0.99
 	enginerpm *= 0.99
-	#$"../Control/Label".text = str(abs(velocity.normalized()-drift.normalized()) > Vector3(0.1,0,0.1))
-	#$"../Control/Label".text = str(abs(velocity.normalized()-drift.normalized()))
-	if is_on_wall():
-		drift += get_wall_normal()*100
-		enginerpm *= 0.9
-	if Input.is_action_just_pressed("forward"):
-		drift = Vector3.ZERO
+	#$"../Control/DebugLabel".text = str(abs(velocity.normalized()-drift.normalized()) > Vector3(0.1,0,0.1))
+	#$"../Control/DebugLabel".text = str(abs(velocity.normalized()-drift.normalized()))
+	rotation.z = move_toward(rotation.z,turnInput*0.01,delta*0.1)
+	$"../Control/Speedometer".text = str(abs(speed))+" KM/H"
 
 func _physics_process(delta: float) -> void:
 	getInput(delta)
